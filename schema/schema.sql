b06c2a9b0f15c6ad7c5255d35d353c02338a0706
Wolfgang Gassler <wolfgang@gassler.org>
2022-09-28 11:15:01 +0200

    changed to WS to handle NULL user-agent values

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
   account_id INTEGER NOT NULL,
-  ev_userhash CHAR(64) AS (SHA2(CONCAT(JSON_UNQUOTE(ev_raw->"$.ip"),JSON_UNQUOTE(ev_raw->'$."user-agent"')), 256)) STORED,
+  ev_userhash CHAR(64) AS (SHA2(CONCAT_WS("",JSON_UNQUOTE(ev_raw->"$.ip"),JSON_UNQUOTE(ev_raw->'$."user-agent"')), 256)) STORED,
   ev_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   ev_raw JSON,
   PRIMARY KEY (account_id, ev_timestamp)
