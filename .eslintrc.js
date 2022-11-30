fe19bd378cb7fd2a43711c543626f8ba053845d4
Matthias <matthias-endler@gmx.net>
2022-11-30 07:19:51 +0100

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint', 'prettier'],
     root: true,
