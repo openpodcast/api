138ccfb504b0aee0c5e31a904a0c4816746e5133
Wolfgang Gassler <wolfgang@gassler.org>
2022-12-21 16:12:49 +0100

    add _FILE variables and separated MYSQL_VARS support

---
+++ src/config.ts
@@ -0,0 +1,56 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
+
+class Config {
+    constructor() {
+        dotenv.config()
+    }
+
+    // reads a value from an environment variable or a file in the format <envVar>_FILE
+    readStringFromEnvOrFile(
+        envVar: string,
+        defaultValue: string | undefined
+    ): string | undefined {
+        let value = process.env[envVar]
+        if (value === undefined) {
+            const filePath = process.env[`${envVar}_FILE`]
+            if (filePath && fs.existsSync(filePath)) {
+                value = fs.readFileSync(filePath, 'utf8')
+            }
+        }
+        if (value === undefined) {
+            return defaultValue
+        }
+        return value
+    }
+
+    getMySQLConnectionString(): string | undefined {
+        let connectionString = process.env.DB_CONNECTION_STRING
+        if (connectionString === undefined) {
+            //let's build the connection string from the individual env vars
+            const host = this.readStringFromEnvOrFile('MYSQL_HOST', 'localhost')
+            const port = this.readStringFromEnvOrFile('MYSQL_PORT', '3306')
+            const user = this.readStringFromEnvOrFile('MYSQL', undefined)
+            const password = this.readStringFromEnvOrFile(
+                'MYSQL_PASSWORD',
+                undefined
+            )
+            const database = this.readStringFromEnvOrFile(
+                'MYSQL_DATABASE',
+                undefined
+            )
+            const options = this.readStringFromEnvOrFile('MYSQL_OPTIONS', '')
+
+            if (host && user && password && database && port) {
+                connectionString = `mysql://${user}:${password}@${host}:${port}/${database}${options}}`
+            } else {
+                throw new Error(
+                    'MySQL connection string not defined or could not be built from environment variables'
+                )
+            }
+        }
+        return connectionString
+    }
+}
+
+export { Config }
