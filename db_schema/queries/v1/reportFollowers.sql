ef2c2c91a96d2c2507b47cd771c79a84ed1f400b
Matthias <matthias-endler@gmx.net>
2023-04-14 14:05:32 +0200

    Add followers endpoint for reports

---
+++ db_schema/queries/v1/reportFollowers.sql
@@ -0,0 +1,10 @@
+SELECT 
+  atf_date as `date`,
+  atf_totalfollowers as apple_total_followers,
+  spf_count as spotify_total_followers
+FROM appleTrendsPodcastFollowers as apple JOIN spotifyPodcastFollowers as spotify
+ON (atf_date = spf_date AND apple.account_id = spotify.account_id)
+WHERE
+  atf_date >= @start
+  AND atf_date <= @end
+  AND apple.account_id = @podcast_id;
