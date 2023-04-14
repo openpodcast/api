14320d0edc65d283df2eac21fcd7f400c5c22aa5
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-14 18:51:20 +0200

    podcastFollowers view

--- db_schema/views.sql
+++ db_schema/views.sql
@@ -29,3 +29,40 @@ SELECT account_id, spa_date as `date`, "female" as gender, spa_gender_female as 
 UNION SELECT account_id, spa_date as `date`,  "male" as gender, spa_gender_male as listeners, spa_facet as age_group FROM data 
 UNION SELECT account_id, spa_date as `date`, "non-binary" as gender, spa_gender_non_binary as listeners, spa_facet as age_group FROM data 
 UNION SELECT account_id, spa_date as `date`, "not-specified" as gender, spa_gender_not_specified as listeners, spa_facet as age_group FROM data;
+
+
+-- Followers gain/growth
+CREATE OR REPLACE VIEW podcastFollowers AS
+WITH
+spotify as (
+  SELECT
+    today.account_id,
+    today.spf_date as `date`,
+    today.spf_count as total_followers,
+    today.spf_count-yesterday.spf_count as `change`
+  FROM
+    spotifyPodcastFollowers as today LEFT JOIN spotifyPodcastFollowers as yesterday 
+    ON (yesterday.spf_date=DATE_SUB(today.spf_date, INTERVAL 1 DAY) AND today.account_id=yesterday.account_id)
+),
+apple as (
+  SELECT
+    today.account_id, today.atf_date as `date`,
+    today.atf_totalfollowers as total_followers,
+    today.atf_totalfollowers-yesterday.atf_totalfollowers as `change`
+  FROM
+    appleTrendsPodcastFollowers as today LEFT JOIN appleTrendsPodcastFollowers as yesterday
+    ON (yesterday.atf_date=DATE_SUB(today.atf_date, INTERVAL 1 DAY) AND today.account_id=yesterday.account_id)
+)
+
+SELECT
+  spotify.account_id, spotify.date,
+  spotify.total_followers as spotify_total_followers,
+  spotify.change as spotify_change_to_day_before,
+  apple.total_followers as apple_total_followers,
+  apple.change as apple_change_to_day_before
+FROM
+  spotify JOIN apple
+  ON (spotify.account_id=apple.account_id AND spotify.date=apple.date)
+WHERE
+  spotify.total_followers IS NOT NULL
+  AND apple.total_followers IS NOT NULL;
