1e7c9afe6eb04b620a96214ee613ef47630aa197
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-25 10:50:52 +0200

    start with podcast sources for now

--- db_schema/auth.sql
+++ db_schema/auth.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS podcastSources (
   source_name ENUM('spotify','apple'),
   source_podcast_id VARCHAR(64) NOT NULL,
   -- keys are stored in json format and are encrypted by the client
-  source_access_keys_encrypted TEXT NOT NULL,
+  source_access_keys_encrypted JSON NOT NULL,
   PRIMARY KEY (account_id, source_name)
 );
 
@@ -14,31 +14,35 @@ ALTER TABLE podcastSources
 ADD CONSTRAINT podcastSources_account_id_fk
 FOREIGN KEY (account_id) REFERENCES openpodcast.podcasts(account_id) ON DELETE CASCADE;
 
--- table to manage all keys for the different apis
-CREATE TABLE IF NOT EXISTS apiKeys (
-  key_id INTEGER NOT NULL AUTO_INCREMENT,
-  -- SHA256 hash of the key, no salting as we are storing long random keys
-  -- and the hash is used to lookup the key which would be more complicated using salting
-  -- and wouldn't increase security a lot
-  key_hash CHAR(64) NOT NULL,
-  PRIMARY KEY (key_hash),
-  INDEX id_hash_idx (key_id, key_hash)
-);
+-- TODO: api permissions are not used yet
+-- access to DB user of api should be restricted to readable
 
--- define permissions for the different api keys
-CREATE TABLE IF NOT EXISTS apiKeysPermissions (
-  key_id INTEGER NOT NULL,
-  account_id INTEGER NOT NULL, -- is podcast id and should be renamed
-  permission_pushevents BOOLEAN NOT NULL DEFAULT FALSE,
-  permission_api_read BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (key_id, account_id),
-  INDEX (account_id)
-);
+-- table to manage all keys of all internal apis
+-- CREATE TABLE IF NOT EXISTS apiKeys (
+--   key_id INTEGER NOT NULL AUTO_INCREMENT,
+--   -- SHA256 hash of the key, no salting as we are storing long random keys
+--   -- and the hash is used to lookup the key which would be more complicated using salting
+--   -- and wouldn't increase security a lot
+--   key_hash CHAR(64) NOT NULL,
+--   PRIMARY KEY (key_hash),
+--   INDEX id_hash_idx (key_id, key_hash)
+-- );
 
-ALTER TABLE apiKeysPermissions
-ADD CONSTRAINT apiKeysPermissions_key_id_fk
-FOREIGN KEY (key_id) REFERENCES apiKeys(key_id) ON DELETE CASCADE;
+-- define permissions of different api keys
 
-ALTER TABLE apiKeysPermissions
-ADD CONSTRAINT apiKeysPermissions_account_id_fk
-FOREIGN KEY (account_id) REFERENCES openpodcast.podcasts(account_id) ON DELETE CASCADE;
+-- CREATE TABLE IF NOT EXISTS apiKeysPermissions (
+--   key_id INTEGER NOT NULL,
+--   account_id INTEGER NOT NULL, -- is podcast id and should be renamed
+--   permission_pushevents BOOLEAN NOT NULL DEFAULT FALSE,
+--   permission_api_read BOOLEAN NOT NULL DEFAULT FALSE,
+--   PRIMARY KEY (key_id, account_id),
+--   INDEX (account_id)
+-- );
+
+-- ALTER TABLE apiKeysPermissions
+-- ADD CONSTRAINT apiKeysPermissions_key_id_fk
+-- FOREIGN KEY (key_id) REFERENCES apiKeys(key_id) ON DELETE CASCADE;
+
+-- ALTER TABLE apiKeysPermissions
+-- ADD CONSTRAINT apiKeysPermissions_account_id_fk
+-- FOREIGN KEY (account_id) REFERENCES openpodcast.podcasts(account_id) ON DELETE CASCADE;
