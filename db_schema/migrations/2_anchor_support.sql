5835778d24d0e60d110ba75bcf564d24d0f5ed22
Matthias <matthias-endler@gmx.net>
2023-05-14 21:21:48 +0200

    Update migration with all tables

--- db_schema/migrations/2_anchor_support.sql
+++ db_schema/migrations/2_anchor_support.sql
@@ -5,68 +5,130 @@ CREATE TABLE IF NOT EXISTS anchorAudienceSize (
   PRIMARY KEY (account_id, aas_date)
 );
 
-CREATE TABLE IF NOT EXISTS anchorEpisodeMetadata (
+CREATE TABLE IF NOT EXISTS anchorAggregatedPerformance (
   account_id INTEGER NOT NULL,
-  episode_id VARCHAR(128) NOT NULL,
-  ep_name VARCHAR(2048) NOT NULL,
-  ep_url VARCHAR(2048),
-  ep_artwork_url VARCHAR(2048),
-  ep_release_date DATE,
-  ep_description TEXT,
-  ep_explicit BOOLEAN,
-  ep_duration INTEGER,
-  ep_language VARCHAR(100),
-  PRIMARY KEY (account_id, episode_id)
+  aap_date DATE NOT NULL,
+  aap_percentile25 INTEGER NOT NULL,
+  aap_percentile50 INTEGER NOT NULL,
+  aap_percentile75 INTEGER NOT NULL,
+  aap_percentile100 INTEGER NOT NULL,
+  aap_average_listen_seconds INTEGER NOT NULL,
+  PRIMARY KEY (account_id, aap_date)
 );
 
-CREATE TABLE IF NOT EXISTS anchorEpisodeDetails (
+CREATE TABLE IF NOT EXISTS anchorEpisodePerformance (
   account_id INTEGER NOT NULL,
-  episode_id VARCHAR(128) NOT NULL,
-  aed_date DATE NOT NULL,
-  aed_playscount INTEGER NOT NULL,
-  aed_totaltimelistened BIGINT NOT NULL,
-  aed_uniqueengagedlistenerscount INTEGER NOT NULL,
-  aed_uniquelistenerscount INTEGER NOT NULL,
-  aed_play_histogram JSON,
-  aed_play_top_cities JSON,
-  aed_play_top_countries JSON,
-  PRIMARY KEY (account_id, episode_id, aed_date)
+  aep_episode_id VARCHAR(128) NOT NULL,
+  aep_date DATE NOT NULL,
+  aep_sample INTEGER NOT NULL,
+  aep_listeners INTEGER NOT NULL,
+  PRIMARY KEY (account_id, aep_episode_id, aep_date)
+
+)
+
+CREATE TABLE IF NOT EXISTS anchorPlays (
+  account_id INTEGER NOT NULL,
+  aep_date DATE NOT NULL,
+  aep_plays INTEGER NOT NULL,
+  PRIMARY KEY (account_id, aep_date)
 );
 
-CREATE TABLE IF NOT EXISTS anchorTrendsEpisodeListeners (
+CREATE TABLE IF NOT EXISTS anchorEpisodePlays (
   account_id INTEGER NOT NULL,
-  episode_id VARCHAR(128) NOT NULL,
-  atl_date DATE NOT NULL,
-  atl_playscount INTEGER NOT NULL,
-  atl_totaltimelistened BIGINT NOT NULL,
-  atl_uniqueengagedlistenerscount INTEGER NOT NULL,
-  atl_uniquelistenerscount INTEGER NOT NULL,
-  PRIMARY KEY (account_id, episode_id, atl_date)
+  aep_episode_id VARCHAR(128) NOT NULL,
+  aep_date DATE NOT NULL,
+  aep_plays INTEGER NOT NULL,
+  PRIMARY KEY (account_id, aep_episode_id, aep_date)
 );
 
-CREATE TABLE IF NOT EXISTS anchorTrendsPodcastListeners (
+CREATE TABLE IF NOT EXISTS anchorPlaysByAgeRange (
   account_id INTEGER NOT NULL,
-  atl_date DATE NOT NULL,
-  atl_playscount INTEGER NOT NULL,
-  atl_totaltimelistened BIGINT NOT NULL,
-  atl_uniqueengagedlistenerscount INTEGER NOT NULL,
-  atl_uniquelistenerscount INTEGER NOT NULL,
-  PRIMARY KEY (account_id, atl_date)
+  apbar_date DATE NOT NULL,
+  apbar_age_range VARCHAR(128) NOT NULL,
+  apbar_plays_percent INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apbar_date, apbar_age_range)
 );
 
-CREATE TABLE IF NOT EXISTS anchorTrendsPodcastListeningTimeFollowerState (
+CREATE TABLE IF NOT EXISTS anchorPlaysByApp (
   account_id INTEGER NOT NULL,
-  atf_date DATE NOT NULL,
-  atf_totaltimelistened_followers BIGINT NOT NULL,
-  atf_totaltimelistened_nonfollowers BIGINT NOT NULL,
-  PRIMARY KEY (account_id, atf_date)
+  apba_date DATE NOT NULL,
+  apba_app VARCHAR(128) NOT NULL,
+  apba_plays_percent INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apba_date, apba_app)
 );
 
-CREATE TABLE IF NOT EXISTS anchorTrendsPodcastFollowers (
+CREATE TABLE IF NOT EXISTS anchorPlaysByDevice(
   account_id INTEGER NOT NULL,
-  atf_date DATE NOT NULL,
-  atf_totalfollowers INTEGER NOT NULL,
-  atf_gained INTEGER NOT NULL,
-  atf_lost INTEGER NOT NULL,
-  PRIMARY KEY (account_id, atf_date)
+  apbd_date DATE NOT NULL,
+  apbd_device VARCHAR(128) NOT NULL,
+  apbd_plays_percent INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apbd_date, apbd_device)
 );
+
+CREATE TABLE IF NOT EXISTS anchorPlaysByEpisode (
+  account_id INTEGER NOT NULL,
+  apbe_date DATE NOT NULL,
+  apbe_episode_id VARCHAR(128) NOT NULL,
+  apbe_plays INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apbe_date, apbe_episode_id)
+);
+
+CREATE TABLE IF NOT EXISTS anchorPlaysByGender (
+  account_id INTEGER NOT NULL,
+  apbg_date DATE NOT NULL,
+  apbg_gender VARCHAR(128) NOT NULL,
+  apbg_plays_percent INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apbg_date, apbg_gender)
+);
+
+CREATE TABLE IF NOT EXISTS anchorPlaysByGeo (
+  account_id INTEGER NOT NULL,
+  apbg_date DATE NOT NULL,
+  apbg_geo VARCHAR(128) NOT NULL,
+  apbg_plays_percent INTEGER NOT NULL,
+  PRIMARY KEY (account_id, apbg_date, apbg_geo)
+);
+
+CREATE TABLE IF NOT EXISTS anchorPodcastEpisodes (
+    account_id INTEGER NOT NULL,
+    ape_podcast_id VARCHAR(128) NOT NULL,
+    ape_episode_id VARCHAR(128) NOT NULL,
+    ape_date DATETIME NOT NULL,
+    ape_title VARCHAR(255) NOT NULL,
+    ape_description TEXT,
+    ape_url VARCHAR(512) NOT NULL,
+    ape_tracked_url VARCHAR(512),
+    ape_episode_image VARCHAR(512),
+    ape_share_link_path VARCHAR(512) NOT NULL,
+    ape_share_link_embed_path VARCHAR(512) NOT NULL,
+    ape_ad_count INTEGER NOT NULL,
+    ape_created DATETIME NOT NULL,
+    ape_duration BIGINT NOT NULL,
+    ape_hour_offset INTEGER NOT NULL,
+    ape_is_deleted BOOLEAN NOT NULL,
+    ape_is_published BOOLEAN NOT NULL,
+    PRIMARY KEY (account_id, ape_episode_id)
+);
+
+CREATE TABLE IF NOT EXISTS anchorTotalPlays (
+  account_id INTEGER NOT NULL,
+  atp_date DATE NOT NULL,
+  atp_plays INTEGER NOT NULL,
+  PRIMARY KEY (account_id, atp_date)
+);
+
+CREATE TABLE IF NOT EXISTS anchorTotalPlaysByEpisode (
+    account_id INTEGER NOT NULL,
+    atpbe_date DATE NOT NULL,
+    atpbe_episode_id VARCHAR(255) NOT NULL,
+    atpbe_plays INTEGER NOT NULL,
+    PRIMARY KEY (account_id, atpbe_date, atpbe_episode_id)
+);
+
+CREATE TABLE IF NOT EXISTS anchorUniqueListeners (
+    account_id INTEGER NOT NULL,
+    aul_date DATE NOT NULL,
+    aul_unique_listeners INTEGER NOT NULL,
+    PRIMARY KEY (account_id, aul_date)
+);
+
