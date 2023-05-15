5fc3eee5211eb4514451bc906076fd5d7e5adfee
Matthias <matthias-endler@gmx.net>
2023-05-15 13:23:24 +0200

    Remove unneeded padding

--- src/db/AnchorRepository.ts
+++ src/db/AnchorRepository.ts
@@ -40,8 +40,8 @@ class AnchorRepository {
 
     getDateDBString(date: Date): string {
         const year = date.getFullYear()
-        const month = String(date.getMonth() + 1).padStart(2, '0')
-        const day = String(date.getDate()).padStart(2, '0')
+        const month = String(date.getMonth() + 1)
+        const day = String(date.getDate())
 
         return `${year}-${month}-${day}`
     }
