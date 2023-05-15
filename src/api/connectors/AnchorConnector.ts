e7bc94c0ca3ef1094c6e52c0bc345c1bd7216b58
Matthias <matthias-endler@gmx.net>
2023-05-15 14:06:27 +0200

    switch-case -> if

--- src/api/connectors/AnchorConnector.ts
+++ src/api/connectors/AnchorConnector.ts
@@ -66,195 +66,170 @@ class AnchorConnector implements ConnectorHandler {
         accountId: number,
         rawPayload: ConnectorPayload
     ): Promise<void> | never {
+        const endpoint = rawPayload.meta.endpoint
+
+        if (endpoint === undefined) {
+            throw new PayloadError('missing endpoint')
+        }
+
         // ConnectorPayload is for compatibility with the other connectors
         // AnchorConnectorPayload is the actual payload type for Anchor.
         // We need to convert the ConnectorPayload to AnchorConnectorPayload
         const payload = rawPayload as AnchorConnectorPayload
 
-        switch (rawPayload.meta.endpoint) {
-            case 'audienceSize':
-                // The schema ensures that we only have one row
-                validateJsonApiPayload(audienceSizeSchema, rawPayload)
+        if (endpoint == 'audienceSize') {
+            // The schema ensures that we only have one row
+            validateJsonApiPayload(audienceSizeSchema, rawPayload)
 
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
 
-                await this.repo.storeAudienceSize(
-                    accountId,
-                    payload.data.data as RawAnchorAudienceSizeData
-                )
-                break
+            await this.repo.storeAudienceSize(
+                accountId,
+                payload.data.data as RawAnchorAudienceSizeData
+            )
+        } else if (endpoint == 'aggregatedPerformance') {
+            validateJsonApiPayload(aggregatedPerformanceSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
 
-            case 'aggregatedPerformance':
-                validateJsonApiPayload(aggregatedPerformanceSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
+            await this.repo.storeAggregatedPerformance(
+                accountId,
+                payload.data.data as RawAnchorAggregatedPerformanceData
+            )
+        } else if (endpoint == 'episodePerformance') {
+            validateJsonApiPayload(episodePerformanceSchema, rawPayload)
+            if (payload.meta.episode === undefined) {
+                throw new PayloadError('missing episode id')
+            }
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storeEpisodePerformance(
+                accountId,
+                payload.meta.episode,
+                payload.data.data as RawAnchorEpisodePerformanceData
+            )
+        } else if (endpoint == 'episodePlays') {
+            validateJsonApiPayload(episodePlaysSchema, rawPayload)
+            if (payload.meta.episode === undefined) {
+                throw new PayloadError('missing episode id')
+            }
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storeEpisodePlays(
+                accountId,
+                payload.meta.episode,
+                payload.data.data as RawAnchorPlaysByEpisodeData
+            )
+        } else if (endpoint == 'plays') {
+            validateJsonApiPayload(playsSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlays(
+                accountId,
+                payload.data.data as RawAnchorPlaysData
+            )
+        } else if (endpoint == 'playsByAgeRange') {
+            validateJsonApiPayload(playsByAgeRangeSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByAgeRange(
+                accountId,
+                payload.data.data as RawAnchorPlaysByAgeRangeData
+            )
+        } else if (endpoint == 'playsByApp') {
+            validateJsonApiPayload(playsByAppSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByApp(
+                accountId,
+                payload.data.data as RawAnchorPlaysByAppData
+            )
+        } else if (endpoint == 'playsByDevice') {
+            validateJsonApiPayload(playsByDeviceSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByDevice(
+                accountId,
+                payload.data.data as RawAnchorPlaysByDeviceData
+            )
+        } else if (endpoint == 'playsByEpisode') {
+            validateJsonApiPayload(playsByEpisodeSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByEpisode(
+                accountId,
+                payload.data.data as RawAnchorPlaysByEpisodeData
+            )
+        } else if (endpoint == 'playsByGender') {
+            validateJsonApiPayload(playsByGenderSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByGender(
+                accountId,
+                payload.data.data as RawAnchorPlaysByGenderData
+            )
+        } else if (endpoint == 'playsByGeo') {
+            validateJsonApiPayload(playsByGeoSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePlaysByGeo(
+                accountId,
+                payload.data.data as RawAnchorPlaysByGeoData
+            )
+        } else if (endpoint == 'podcastEpisode') {
+            validateJsonApiPayload(podcastEpisodeSchema, rawPayload)
 
-                await this.repo.storeAggregatedPerformance(
-                    accountId,
-                    payload.data.data as RawAnchorAggregatedPerformanceData
-                )
-                break
-
-            case 'episodePerformance':
-                validateJsonApiPayload(episodePerformanceSchema, rawPayload)
-                if (payload.meta.episode === undefined) {
-                    throw new PayloadError('missing episode id')
-                }
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storeEpisodePerformance(
-                    accountId,
-                    payload.meta.episode,
-                    payload.data.data as RawAnchorEpisodePerformanceData
-                )
-                break
-
-            case 'episodePlays':
-                validateJsonApiPayload(episodePlaysSchema, rawPayload)
-                if (payload.meta.episode === undefined) {
-                    throw new PayloadError('missing episode id')
-                }
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storeEpisodePlays(
-                    accountId,
-                    payload.meta.episode,
-                    payload.data.data as RawAnchorPlaysByEpisodeData
-                )
-                break
-
-            case 'plays':
-                validateJsonApiPayload(playsSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlays(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysData
-                )
-                break
-
-            case 'playsByAgeRange':
-                validateJsonApiPayload(playsByAgeRangeSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByAgeRange(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByAgeRangeData
-                )
-                break
-
-            case 'playsByApp':
-                validateJsonApiPayload(playsByAppSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByApp(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByAppData
-                )
-                break
-
-            case 'playsByDevice':
-                validateJsonApiPayload(playsByDeviceSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByDevice(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByDeviceData
-                )
-                break
-
-            case 'playsByEpisode':
-                validateJsonApiPayload(playsByEpisodeSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByEpisode(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByEpisodeData
-                )
-                break
-
-            case 'playsByGender':
-                validateJsonApiPayload(playsByGenderSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByGender(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByGenderData
-                )
-                break
-
-            case 'playsByGeo':
-                validateJsonApiPayload(playsByGeoSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePlaysByGeo(
-                    accountId,
-                    payload.data.data as RawAnchorPlaysByGeoData
-                )
-                break
-
-            case 'podcastEpisode':
-                validateJsonApiPayload(podcastEpisodeSchema, rawPayload)
-
-                if (!isRawAnchorPodcastData(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storePodcastEpisodes(
-                    accountId,
-                    payload.data.podcastId,
-                    payload.data as RawAnchorPodcastData
-                )
-                break
-
-            case 'totalPlays':
-                validateJsonApiPayload(totalPlaysSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storeTotalPlays(
-                    accountId,
-                    payload.data.data as RawAnchorTotalPlaysData
-                )
-                break
-
-            case 'totalPlaysByEpisode':
-                validateJsonApiPayload(totalPlaysByEpisodeSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storeTotalPlaysByEpisode(
-                    accountId,
-                    payload.data.data as RawAnchorTotalPlaysByEpisodeData
-                )
-                break
-
-            case 'uniqueListeners':
-                validateJsonApiPayload(uniqueListenersSchema, rawPayload)
-                if (!isDataPayload(payload.data)) {
-                    throw new PayloadError('Incorrect payload data type')
-                }
-                await this.repo.storeUniqueListeners(
-                    accountId,
-                    payload.data.data as RawAnchorUniqueListenersData
-                )
-                break
-
-            default:
-                throw new PayloadError(
-                    `Unknown endpoint in meta: ${rawPayload.meta.endpoint}`
-                )
+            if (!isRawAnchorPodcastData(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storePodcastEpisodes(
+                accountId,
+                payload.data.podcastId,
+                payload.data as RawAnchorPodcastData
+            )
+        } else if (endpoint == 'totalPlays') {
+            validateJsonApiPayload(totalPlaysSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storeTotalPlays(
+                accountId,
+                payload.data.data as RawAnchorTotalPlaysData
+            )
+        } else if (endpoint == 'totalPlaysByEpisode') {
+            validateJsonApiPayload(totalPlaysByEpisodeSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storeTotalPlaysByEpisode(
+                accountId,
+                payload.data.data as RawAnchorTotalPlaysByEpisodeData
+            )
+        } else if (endpoint == 'uniqueListeners') {
+            validateJsonApiPayload(uniqueListenersSchema, rawPayload)
+            if (!isDataPayload(payload.data)) {
+                throw new PayloadError('Incorrect payload data type')
+            }
+            await this.repo.storeUniqueListeners(
+                accountId,
+                payload.data.data as RawAnchorUniqueListenersData
+            )
+        } else {
+            throw new PayloadError(
+                `Unknown endpoint in meta: ${rawPayload.meta.endpoint}`
+            )
         }
     }
 }
