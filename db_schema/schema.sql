2283f8cfcc8d39f203803a297b49e4158b189a5d
Matthias <matthias-endler@gmx.net>
2023-06-05 16:26:20 +0200

    Plays percentage is float, not integer
    The values were all 0. This migration fixes it.

--- db_schema/schema.sql
+++ db_schema/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS migrations (
 -- -----------------------------------------
 -- IMPORTANT: this is the schema version
 -- ID has to be incremented for each change
-INSERT INTO migrations (migration_id, migration_name) VALUES (2, 'anchor support');
+INSERT INTO migrations (migration_id, migration_name) VALUES (4, 'anchor_geo_plays_percent_float');
 -- -----------------------------------------
 
 CREATE TABLE IF NOT EXISTS events (
