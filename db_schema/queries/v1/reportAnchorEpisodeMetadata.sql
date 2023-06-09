07c6566be4e7f470060de3d975bab3f276c23b60
Wolfgang Gassler <wolfgang@gassler.org>
2023-06-09 19:40:46 +0200

    metadata is not historically stored, so no need for start/end

--- db_schema/queries/v1/reportAnchorEpisodeMetadata.sql
+++ db_schema/queries/v1/reportAnchorEpisodeMetadata.sql
@@ -1,13 +1,3 @@
-WITH last_date AS (
-    SELECT
-        MAX(date) as last_date
-    FROM
-        anchorPodcastEpisodes
-    WHERE
-        date >= @start
-        AND date <= @end
-        AND account_id = @podcast_id
-)
 SELECT
     podcast_id,
     episode_id,
@@ -18,8 +8,7 @@ SELECT
 FROM
     anchorPodcastEpisodes
 WHERE
-    date = (SELECT last_date FROM last_date)
-    AND is_published = 1
+    is_published = 1
     AND is_deleted = 0
     AND account_id = @podcast_id
 ORDER BY
