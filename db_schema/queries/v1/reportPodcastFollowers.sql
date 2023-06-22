103b39dfd7406b4e677624a0db9603e449107e63
Matthias <matthias-endler@gmx.net>
2023-06-22 12:48:19 +0200

    Return all dates for followers; not only last one
    This is needed to show follower growth in report

--- db_schema/queries/v1/reportPodcastFollowers.sql
+++ db_schema/queries/v1/reportPodcastFollowers.sql
@@ -1,9 +1,5 @@
-WITH last_date AS (
-  SELECT MAX(date) AS date
-  FROM podcastFollowers
-  WHERE account_id = @podcast_id
-)
 SELECT * FROM podcastFollowers 
 WHERE
-  date = (SELECT date FROM last_date)
-  AND account_id = @podcast_id;
+  account_id = @podcast_id
+  AND date >= @start
+  AND date <= @end
