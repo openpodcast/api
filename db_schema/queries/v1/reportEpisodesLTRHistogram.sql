68397820ec83b7d0c8af981183d31697b3129160
Wolfgang Gassler <wolfgang@gassler.org>
2023-07-06 16:50:38 +0200

    use report endpoint to include more details

---
+++ db_schema/queries/v1/reportEpisodesLTRHistogram.sql
@@ -0,0 +1,23 @@
+SELECT
+  guid,
+  ep_name,
+  spotify.spp_date as date,
+  spp_sample_max as spotify_listeners_max,
+  aed_histogram_max_listeners as apple_listeners_max,
+  spotify.histogram as spotify_histogram,
+  aed_play_histogram as apple_histogram
+FROM
+  episodeMapping
+  JOIN spotifyEpisodePerformance15SecBuckets as spotify
+  JOIN appleEpisodeDetails as apple
+WHERE
+  -- main join criteria
+  spotify.episode_id = episodeMapping.spotify_episode_id
+  AND apple.episode_id = episodeMapping.apple_episode_id
+  -- account id
+  AND episodeMapping.account_id = @podcast_id
+  AND spotify.account_id = @podcast_id
+  AND apple.account_id = @podcast_id
+  -- date criteria
+  AND spotify.spp_date = @end
+  AND apple.aed_date = @end
