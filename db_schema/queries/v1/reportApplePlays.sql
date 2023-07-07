52568f7ea5db149163c3d90e0405fc36087afd79
Wolfgang Gassler <wolfgang@gassler.org>
2023-07-07 17:54:34 +0200

    endpoints for apple followers

--- db_schema/queries/v1/reportApplePlays.sql
+++ db_schema/queries/v1/reportApplePlays.sql
@@ -1,15 +1,13 @@
 SELECT
   account_id as podcast_id,
-  SUM(atl_playscount) as apple_playscount,
-  SUM(atl_totaltimelistened) as apple_totaltimelistened,
-  -- TODO: cannot be summed up as it is unique per day and not for the whole period
-  SUM(atl_uniqueengagedlistenerscount) as apple_uniqueengagedlistenerscount,
-  SUM(atl_uniquelistenerscount) as apple_uniquelistenerscount
+  atl_date as date,
+  atl_playscount as apple_playscount,
+  atl_totaltimelistened as apple_totaltimelistened,
+  atl_uniqueengagedlistenerscount as apple_uniqueengagedlistenerscount,
+  atl_uniquelistenerscount as apple_uniquelistenerscount
 FROM
   appleTrendsPodcastListeners
 WHERE
   atl_date >= @start
   AND atl_date <= @end
-  AND account_id = @podcast_id
-GROUP BY
-  account_id;
+  AND account_id = @podcast_id;
