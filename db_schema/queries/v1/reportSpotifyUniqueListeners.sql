4fa99206ae9dd5cdd6fda8634a1d06643f72c677
Matthias <matthias-endler@gmx.net>
2023-09-19 18:11:56 +0200

    Use last possible date for Spotify Unique Listeners

--- db_schema/queries/v1/reportSpotifyUniqueListeners.sql
+++ db_schema/queries/v1/reportSpotifyUniqueListeners.sql
@@ -1,48 +1,34 @@
-WITH episodeListeners as
+WITH episodeListeners AS
 (
-  SELECT
-    account_id,
-    SUM(n.epm_listeners-o.epm_listeners) as listeners
-  FROM
-    spotifyEpisodeMetadataHistory o 
+    SELECT
+        account_id,
+        SUM(n.epm_listeners - o.epm_listeners) as listeners
+    FROM
+        spotifyEpisodeMetadataHistory o
     JOIN spotifyEpisodeMetadataHistory n USING (account_id, episode_id)
-  WHERE 
-    o.epm_date BETWEEN @start AND @end
-    AND n.epm_date BETWEEN @start AND @end
-    AND account_id = @podcast_id
-  GROUP BY account_id
+    WHERE 
+        o.epm_date = (SELECT MAX(epm_date) FROM spotifyEpisodeMetadataHistory WHERE epm_date <= @start AND account_id = @podcast_id)
+        AND n.epm_date = (SELECT MAX(epm_date) FROM spotifyEpisodeMetadataHistory WHERE epm_date <= @end AND account_id = @podcast_id)
+        AND o.account_id = @podcast_id
+    GROUP BY o.account_id
 ),
-podcastListenersStart as
+podcastListeners AS
 (
-  SELECT
-    account_id,
-    n.spm_listeners-o.spm_listeners as listeners
-  FROM
-    spotifyPodcastMetadata o 
+    SELECT
+        account_id,
+        n.spm_listeners - o.spm_listeners as listeners
+    FROM
+        spotifyPodcastMetadata o
     JOIN spotifyPodcastMetadata n USING (account_id)
-  WHERE
-    o.spm_date BETWEEN @start AND @end
-    AND account_id = @podcast_id
-  ORDER BY o.spm_date ASC
-  LIMIT 1
-),
-podcastListenersEnd as
-(
-  SELECT
-    account_id,
-    n.spm_listeners-o.spm_listeners as listeners
-  FROM
-    spotifyPodcastMetadata o 
-    JOIN spotifyPodcastMetadata n USING (account_id)
-  WHERE
-    n.spm_date BETWEEN @start AND @end
-    AND account_id = @podcast_id
-  ORDER BY n.spm_date DESC
-  LIMIT 1
+    WHERE
+        o.spm_date = (SELECT MAX(spm_date) FROM spotifyPodcastMetadata WHERE spm_date <= @start AND account_id = @podcast_id)
+        AND n.spm_date = (SELECT MAX(spm_date) FROM spotifyPodcastMetadata WHERE spm_date <= @end AND account_id = @podcast_id)
+        AND o.account_id = @podcast_id
+    LIMIT 1
 )
 SELECT
-    e.account_id,
-    e.listeners as unique_podcast_listens,
-    p.listeners as new_unique_podcast_listeners
-FROM episodeListeners e 
-JOIN podcastListenersEnd p USING (account_id)
+    account_id,
+    episodeListeners.listeners as unique_podcast_listens,
+    podcastListeners.listeners as new_unique_podcast_listeners
+FROM episodeListeners 
+JOIN podcastListeners USING (account_id)
