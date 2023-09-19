30fa1bdc3487b3b6d4433ffb67b2e3af06fb0d01
Matthias <matthias-endler@gmx.net>
2023-09-19 15:15:38 +0200

    Fix data retrieval for unique listeners

--- db_schema/queries/v1/reportSpotifyUniqueListeners.sql
+++ db_schema/queries/v1/reportSpotifyUniqueListeners.sql
@@ -1,29 +1,48 @@
 WITH episodeListeners as
-(SELECT
+(
+  SELECT
     account_id,
     SUM(n.epm_listeners-o.epm_listeners) as listeners
-FROM
-  spotifyEpisodeMetadataHistory o JOIN spotifyEpisodeMetadataHistory n USING (account_id, episode_id)
-WHERE 
-  o.epm_date = @start
-  AND n.epm_date = @end
-  AND account_id = @podcast_id
-GROUP BY account_id
+  FROM
+    spotifyEpisodeMetadataHistory o 
+    JOIN spotifyEpisodeMetadataHistory n USING (account_id, episode_id)
+  WHERE 
+    o.epm_date BETWEEN @start AND @end
+    AND n.epm_date BETWEEN @start AND @end
+    AND account_id = @podcast_id
+  GROUP BY account_id
 ),
-podcastListeners as
-(SELECT
+podcastListenersStart as
+(
+  SELECT
     account_id,
     n.spm_listeners-o.spm_listeners as listeners
-FROM
-    spotifyPodcastMetadata o JOIN spotifyPodcastMetadata n USING (account_id)
-WHERE
-    o.spm_date = @start
-    AND n.spm_date = @end
+  FROM
+    spotifyPodcastMetadata o 
+    JOIN spotifyPodcastMetadata n USING (account_id)
+  WHERE
+    o.spm_date BETWEEN @start AND @end
     AND account_id = @podcast_id
-LIMIT 1
+  ORDER BY o.spm_date ASC
+  LIMIT 1
+),
+podcastListenersEnd as
+(
+  SELECT
+    account_id,
+    n.spm_listeners-o.spm_listeners as listeners
+  FROM
+    spotifyPodcastMetadata o 
+    JOIN spotifyPodcastMetadata n USING (account_id)
+  WHERE
+    n.spm_date BETWEEN @start AND @end
+    AND account_id = @podcast_id
+  ORDER BY n.spm_date DESC
+  LIMIT 1
 )
 SELECT
-    account_id,
-    episodeListeners.listeners as unique_podcast_listens,
-    podcastListeners.listeners as new_unique_podcast_listeners
-FROM episodeListeners JOIN podcastListeners USING (account_id)
+    e.account_id,
+    e.listeners as unique_podcast_listens,
+    p.listeners as new_unique_podcast_listeners
+FROM episodeListeners e 
+JOIN podcastListenersEnd p USING (account_id)
