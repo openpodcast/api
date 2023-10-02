14045863d2cc8903f43f4bae7ae2d8dc491492cc
Matthias <matthias-endler@gmx.net>
2023-10-02 14:51:08 +0200

    fix top episodes query

--- db_schema/queries/v1/reportAnchorTopEpisodes.sql
+++ db_schema/queries/v1/reportAnchorTopEpisodes.sql
@@ -8,7 +8,10 @@ FROM
 JOIN
     anchorPodcastEpisodes p ON e.account_id = p.account_id AND e.episode_id = p.episode_id
 JOIN
-    anchorTotalPlaysByEpisode t ON e.account_id = t.account_id AND e.episode_id = t.episode_id
+    -- Anchor uses two different ids for the same episode, we need to map them for the rest of the query
+    anchorEpisodesPage mapping ON e.account_id = mapping.account_id AND e.episode_id = mapping.web_episode_id
+JOIN
+    anchorTotalPlaysByEpisode t ON e.account_id = t.account_id AND mapping.episode_id = t.episode_id
 WHERE 
     e.date >= @start
     AND e.date <= @end
@@ -21,6 +24,6 @@ WHERE
             AND date BETWEEN @start AND @end
     )
 GROUP BY 
-    e.episode_id, p.title
+    e.episode_id, p.title, t.plays
 ORDER BY 
     total_plays DESC
