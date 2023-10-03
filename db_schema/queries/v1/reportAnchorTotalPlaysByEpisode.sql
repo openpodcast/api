9a40ef59af82c38ca803535ff7568beee6c47d1a
Wolfgang Gassler <wolfgang@gassler.org>
2023-10-03 14:54:34 +0200

    fix total number problem (off by one error)

--- db_schema/queries/v1/reportAnchorTotalPlaysByEpisode.sql
+++ db_schema/queries/v1/reportAnchorTotalPlaysByEpisode.sql
@@ -1,13 +1,3 @@
-WITH last_date AS (
-    SELECT 
-        MAX(date) AS max_date
-    FROM 
-        anchorTotalPlaysByEpisode
-    WHERE
-        date >= @start
-        AND date <= @end
-        AND account_id = @podcast_id
-)
 SELECT
     a.account_id,
     a.date,
@@ -16,11 +6,16 @@ SELECT
 FROM
     anchorTotalPlaysByEpisode AS a
 JOIN 
-    last_date AS b ON a.date = b.max_date
-JOIN 
     anchorEpisodesPage e ON a.account_id = e.account_id AND a.episode_id = e.episode_id
 WHERE
     a.account_id = @podcast_id
+
+    AND a.date = (
+        SELECT MAX(date) FROM anchorTotalPlaysByEpisode
+        WHERE account_id = @podcast_id
+        -- as this table stores always the total number of yesterday
+        -- we need to increase the end date by one day
+        AND date BETWEEN @start AND DATE_ADD(@end, INTERVAL 1 DAY)
+    )
 ORDER BY 
     a.plays DESC
-   
