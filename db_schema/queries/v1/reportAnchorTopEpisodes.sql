e51866a626a4de35f9b4780c3fa83bd873862544
Wolfgang Gassler <wolfgang@gassler.org>
2023-10-25 00:36:17 +0200

    fix anchor query to show alltime plays together with time range plays

--- db_schema/queries/v1/reportAnchorTopEpisodes.sql
+++ db_schema/queries/v1/reportAnchorTopEpisodes.sql
@@ -4,18 +4,16 @@ SELECT
     SUM(e.plays) AS total_plays_in_period,
     t.plays AS total_plays
 FROM 
-    anchorEpisodePlays e
-JOIN
-    anchorPodcastEpisodes p ON e.account_id = p.account_id AND e.episode_id = p.episode_id
-JOIN
+    anchorPodcastEpisodes p
     -- Anchor uses two different ids for the same episode, we need to map them for the rest of the query
-    anchorEpisodesPage mapping ON e.account_id = mapping.account_id AND e.episode_id = mapping.web_episode_id
-JOIN
-    anchorTotalPlaysByEpisode t ON e.account_id = t.account_id AND mapping.episode_id = t.episode_id
-WHERE 
-    e.date >= @start
+    JOIN anchorEpisodesPage mapping ON p.account_id = mapping.account_id AND p.episode_id = mapping.web_episode_id
+    JOIN anchorTotalPlaysByEpisode t ON p.account_id = t.account_id AND t.episode_id = mapping.episode_id
+    -- as we might not have plays for all episodes, we use a left join
+    LEFT JOIN anchorEpisodePlays e ON p.account_id = e.account_id AND e.episode_id = mapping.web_episode_id
+WHERE
+    p.account_id = @podcast_id
+    AND e.date >= @start
     AND e.date <= @end
-    AND e.account_id = @podcast_id
     AND t.date = (
         SELECT MAX(date)
         FROM anchorTotalPlaysByEpisode
