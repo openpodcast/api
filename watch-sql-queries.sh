eb49e7118177fefca622ad92f241445bb197dae5
Matthias <matthias-endler@gmx.net>
2023-10-30 01:16:06 +0100

    Use fswatch to watch file changes
    Please test on Linux

--- watch-sql-queries.sh
+++ watch-sql-queries.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check if inotifywait is available
-if ! command -v inotifywait &> /dev/null; then
-    echo "Error: inotifywait command not found. Please install inotify-tools."
+# Check if fswatch is available
+if ! command -v fswatch &> /dev/null; then
+    echo "Error: fswatch command not found. Please install it"
     exit 1
 fi
 
@@ -14,6 +14,19 @@ fi
 
 # Podcast ID
 PODCAST_ID="$1"
+
+# If on macOS, use gdate instead of date
+if [ "$(uname)" == "Darwin" ]; then
+    if ! command -v gdate &> /dev/null; then
+        echo "Error: gdate command not found. Please install it"
+        exit 1
+    fi
+
+    # Use gdate instead of date
+    date() {
+        gdate "$@"
+    }
+fi
 
 # Default start and end dates (1st of last month to last day of last month)
 DEFAULT_START_DATE=$(date -d "$(date +%Y-%m-01) -1 month" +%Y-%m-%d)
@@ -30,8 +43,11 @@ echo "Watching directory: $WATCH_DIR"
 echo "Using podcast ID: $PODCAST_ID in $START_DATE to $END_DATE"
 
 while true; do
-    # Use inotifywait to monitor file changes in the directory
-    file=$(inotifywait -q -e modify --format "%w%f" "$WATCH_DIR")
+    # # Use inotifywait to monitor file changes in the directory
+    # file=$(inotifywait -q -e modify --format "%w%f" "$WATCH_DIR")
+
+    # Use fswatch to monitor file changes in the directory
+    file=$(fswatch -1 "$WATCH_DIR")
 
     # Use sed to replace all placeholders occurences @start, @end, @podcast_id
     content=$(sed -e "s/@start/\"$START_DATE\"/g" -e "s/@end/\"$END_DATE\"/g" -e "s/@podcast_id/$PODCAST_ID/g" "$file")
