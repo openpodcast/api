4672f4ed32797dda7e04e277e92cc75bcf46c145
Matthias <matthias-endler@gmx.net>
2023-10-31 13:31:21 +0100

    Add Podigee support
    * [X] Provide fixtures
    * [X] Update schemas
    * [ ] Add code
    * [ ] Testing

---
+++ src/db/PodigeeRepository.ts
@@ -0,0 +1,57 @@
+import { RawPodigeePodcastAnalyticsData } from '../types/provider/podigee'
+
+const getDateDBString = (date: Date): string => {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1)
+    const day = String(date.getDate())
+
+    return `${year}-${month}-${day}`
+}
+
+const getTodayDBString = (): string => {
+    return getDateDBString(new Date())
+}
+
+// Convert unix timestamp in seconds or milliseconds to Date string in
+// format YYYY-MM-DD
+const getDateFromTimestamp = (timestamp: number | string): string => {
+    // Convert milliseconds to seconds if necessary
+    if (timestamp.toString().length === 13) {
+        timestamp = Number(timestamp) / 1000
+    }
+    const date = new Date(Number(timestamp) * 1000)
+    return getDateDBString(date)
+}
+
+class PodigeeRepository {
+    pool
+
+    constructor(pool: any) {
+        this.pool = pool
+    }
+
+    async storePodcastAnalytics(
+        accountId: number,
+        data: RawPodigeePodcastAnalyticsData
+    ): Promise<any> {
+        throw new Error('Method not implemented.')
+
+        // // audienceSize.rows is an array with a single element
+        // const audienceSize = data.rows[0]
+
+        // const replaceStmt = `REPLACE INTO anchorAudienceSize (
+        //     account_id,
+        //     date,
+        //     audience_size
+        //     ) VALUES
+        //     (?,?,?)`
+
+        // return await this.pool.query(replaceStmt, [
+        //     accountId,
+        //     getTodayDBString(),
+        //     audienceSize,
+        // ])
+    }
+}
+
+export { PodigeeRepository }
