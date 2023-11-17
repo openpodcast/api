c416cf9504f21e0f303987a567331c7b1fc1c01d
Wolfgang Gassler <wolfgang@gassler.org>
2023-11-17 12:44:46 +0100

    fix report query

--- db_schema/queries/v1/reportPodcastAge.sql
+++ db_schema/queries/v1/reportPodcastAge.sql
@@ -1,34 +1,26 @@
--- This is the same endpoint as podcastAge,
--- but it only gets the latest date as it is used for the podcast report
-
-WITH latest_date as
-(
-  SELECT
-  MAX(spa_date) as latest_date
-  FROM spotifyPodcastAggregate
-  WHERE
-  spa_facet_type="age"
-  AND account_id = @podcast_id
-  AND spa_date >= @start
-  AND spa_date <= @end
-),
+WITH 
 data as
 (
   SELECT
-  spa_facet,
-  spa_gender_female+spa_gender_male+spa_gender_non_binary+spa_gender_not_specified as listeners,
-  spa_date
+  spa_facet as age_group,
+  SUM(spa_gender_female+spa_gender_male+spa_gender_non_binary+spa_gender_not_specified) listeners,
+  SUM(spa_gender_female) as female,
+  SUM(spa_gender_male) as male,
+  SUM(spa_gender_non_binary) as non_binary,
+  SUM(spa_gender_not_specified) as not_specified
   FROM spotifyPodcastAggregate
   WHERE
   spa_facet_type="age"
   AND account_id = @podcast_id
   AND spa_date >= @start
   AND spa_date <= @end
+  GROUP BY spa_facet
 )
 SELECT
-spa_date as `date`,
-spa_facet as age_group,
-listeners,
-ROUND(listeners/(SELECT SUM(listeners) FROM data WHERE spa_date=final.spa_date)*100,2) as percent
-FROM data as final
-WHERE spa_date = (SELECT latest_date FROM latest_date)
+*,
+SUM(listeners) OVER() as total_listeners,
+ROUND(female/(SUM(listeners) OVER())*100, 2) as female_percent,
+ROUND(male/(SUM(listeners) OVER())*100, 2) as male_percent,
+ROUND(non_binary/(SUM(listeners) OVER())*100, 2) as non_binary_percent,
+ROUND(not_specified/(SUM(listeners) OVER())*100, 2) as not_specified_percent
+FROM data
