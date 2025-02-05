d44e556374ed348298617a342f003ac506c251e7
Wolfgang Gassler <wolfgang@gassler.org>
2025-02-05 23:31:30 +0100

    Increase JSON payload limit from 2mb to 5mb
    openpodcast/report#56

--- src/index.ts
+++ src/index.ts
@@ -99,7 +99,7 @@ const app: Express = express()
 const port = config.getExpressPort()
 
 // extract json payload from body automatically
-app.use(bodyParser.json({ limit: '2mb' }))
+app.use(bodyParser.json({ limit: '5mb' }))
 
 app.use(express.static('public'))
 app.set('view engine', 'handlebars')
