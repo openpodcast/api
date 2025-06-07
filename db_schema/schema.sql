4d3b5e5447835b625c01ebda558a3062de8cd810
Matthias Endler <matthias@endler.dev>
2025-06-07 10:11:28 +0200

    Generic hoster support (#169)
    * Add Podigee support
    
    * [X] Provide fixtures
    * [X] Update schemas
    * [ ] Add code
    * [ ] Testing
    
    * wip
    
    * add test
    
    * Work on hoster support
    
    * rename more files
    
    * fix import as the prophet said
    
    * wording
    
    * fix imports
    
    * fix SQL syntax
    
    * Enable test
    
    * Add show and hosterId
    
    * - using provider field to define host (e.g. podigee)
    - store methods in repo for generic hosters
    - moved defintion of supported hosters from mysql to js as a static map
    
    * tests
    
    * Add support for generic hosters migration
    
    * fix lint issues
    
    * Adjust fixtures and code
    
    * Re-enable all tests
    
    * cleanup
    
    * Remove range from fixtures and schema.
    
    Turns out we didn't use it anywhere except for aggregate, in which
    case we also had it in the payload
    
    * Revert "speed up anchor histogram query from timeout to 0.5 seconds"
    
    This reverts commit 1ecf3fb756dab152c6a74e4df72764e035d664b8.
    
    * Update src/types/provider/hoster.ts
    
    * cleanup
    
    ---------
    
    Co-authored-by: Wolfgang Gassler <wolfgang@gassler.org>

--- db_schema/schema.sql
+++ db_schema/schema.sql
@@ -696,5 +696,60 @@ CREATE TABLE IF NOT EXISTS anchorUniqueListeners (
     PRIMARY KEY (account_id, date)
 );
 
+-- Support for generic hosters, e.g. Podigee (migration 12)
 
+CREATE TABLE IF NOT EXISTS hosterPodcastMetadata (
+  account_id INTEGER NOT NULL,
+  hoster_id MEDIUMINT UNSIGNED NOT NULL,
+  date DATE NOT NULL,
+  name VARCHAR(2048) NOT NULL,
+  -- Date is not part of the primary key
+  -- because we only want to store the latest data
+  PRIMARY KEY (account_id, hoster_id)
+);
+
+CREATE TABLE IF NOT EXISTS hosterPodcastMetrics (
+  account_id INTEGER NOT NULL,
+  hoster_id MEDIUMINT UNSIGNED NOT NULL,
+  start DATETIME NOT NULL,
+  end DATETIME NOT NULL,
+  dimension ENUM(
+    'downloads',
+    'platforms',
+    'clients',
+    'sources'
+  ) NOT NULL,
+  subdimension VARCHAR(255) DEFAULT '' NOT NULL,
+  value INTEGER NOT NULL,
+  PRIMARY KEY (account_id, hoster_id, start, end, dimension, subdimension)
+);
+
+
+CREATE TABLE IF NOT EXISTS hosterEpisodeMetadata (
+  account_id INTEGER NOT NULL,
+  hoster_id MEDIUMINT UNSIGNED NOT NULL,
+  episode_id VARCHAR(128) NOT NULL,
+  ep_name VARCHAR(2048) NOT NULL,
+  ep_url VARCHAR(2048),
+  -- Date could be NULL for unpublished episodes
+  ep_release_date DATETIME,
+  PRIMARY KEY (account_id, hoster_id, episode_id)
+);
+
+CREATE TABLE IF NOT EXISTS hosterEpisodeMetrics (
+  account_id INTEGER NOT NULL,
+  hoster_id MEDIUMINT UNSIGNED NOT NULL,
+  episode_id VARCHAR(128) NOT NULL,
+  start DATETIME NOT NULL,
+  end DATETIME NOT NULL,
+  dimension ENUM(
+    'downloads',
+    'platforms',
+    'clients',
+    'sources'
+  ) NOT NULL,
+  subdimension VARCHAR(255) DEFAULT '' NOT NULL,
+  value INTEGER NOT NULL,
+  PRIMARY KEY (account_id, hoster_id, episode_id, start, end, dimension, subdimension)
+);
 
