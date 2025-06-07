732cd663d2bcd5cfcd370979bb0c52af34f1b8ae
Matthias Endler <matthias@endler.dev>
2025-06-07 10:11:28 +0200

    Generic hoster support (#169)
    * Add Podigee support
    
    * [X] Provide fixtures
    * [X] Update schemas
    * [ ] Add code
    * [ ] Testing
    
    * wip
    
    * add test
    
    * Work on hoster support
    
    * rename more files
    
    * fix import as the prophet said
    
    * wording
    
    * fix imports
    
    * fix SQL syntax
    
    * Enable test
    
    * Add show and hosterId
    
    * - using provider field to define host (e.g. podigee)
    - store methods in repo for generic hosters
    - moved defintion of supported hosters from mysql to js as a static map
    
    * tests
    
    * Add support for generic hosters migration
    
    * fix lint issues
    
    * Adjust fixtures and code
    
    * Re-enable all tests
    
    * cleanup
    
    * Remove range from fixtures and schema.
    
    Turns out we didn't use it anywhere except for aggregate, in which
    case we also had it in the payload
    
    * Revert "speed up anchor histogram query from timeout to 0.5 seconds"
    
    This reverts commit 1ecf3fb756dab152c6a74e4df72764e035d664b8.
    
    * Update src/types/provider/hoster.ts
    
    * cleanup
    
    ---------
    
    Co-authored-by: Wolfgang Gassler <wolfgang@gassler.org>

--- db_schema/queries/v1/reportAnchorAvgEpisodeLTRHistogram.sql
+++ db_schema/queries/v1/reportAnchorAvgEpisodeLTRHistogram.sql
@@ -1,22 +1,26 @@
-WITH
-latestValidDate as ( SELECT MAX(date) as d FROM anchorEpisodePerformance WHERE account_id = @podcast_id),  
-baseData as (SELECT * FROM anchorEpisodePerformance WHERE	date = (SELECT d	FROM latestValidDate) AND account_id = @podcast_id)
-
+WITH latestValidDate as ( SELECT MAX(date) FROM anchorEpisodePerformance WHERE account_id = @podcast_id)  
 SELECT
 	account_id,
 	sec,
 	round(avg(JSON_EXTRACT(samples, concat('$."', sec, '"')) / max_listeners) * 100, 2) as average_listener_percent
 FROM
-	baseData
+	anchorEpisodePerformance
 CROSS JOIN
 	 json_table(
 	        json_keys(samples)
 	        ,
 	'$[*]' columns(sec int path '$')
 	    ) samples
+WHERE
+	date = (
+	SELECT
+		date
+	FROM
+		latestValidDate)
+	and account_id = @podcast_id
 GROUP BY
 	account_id,
 	sec
 	-- cut off tail of the average values, for which we don't have at least 3 episodes
 HAVING
-	count(*) > 2 
+	count(*) > 2
