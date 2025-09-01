a15f0faf2f1d53fa8521fe8d0821987af24205eb
Matthias <matthias@endler.dev>
2025-09-01 18:27:34 +0200

    Get last values by source_id

--- db_schema/queries/v1/spotifyImpressionsSources.sql
+++ db_schema/queries/v1/spotifyImpressionsSources.sql
@@ -1,16 +1,16 @@
 -- Gets impressions breakdown by source (HOME, SEARCH, LIBRARY, OTHER)
 -- Returns data for the most recent 30-day period within the requested date range
-SELECT
-	account_id,
-	date_start,
-	date_end,
-	source_id,
-	impression_count
+
+SELECT DISTINCT
+    account_id,
+    FIRST_VALUE(date_start) OVER (PARTITION BY source_id ORDER BY date_start DESC) as date_start,
+    FIRST_VALUE(date_end) OVER (PARTITION BY source_id ORDER BY date_start DESC) as date_end,
+    source_id,
+    FIRST_VALUE(impression_count) OVER (PARTITION BY source_id ORDER BY date_start DESC) as impression_count
 FROM
-	spotifyImpressionsSources
+    spotifyImpressionsSources
 WHERE
-	account_id = @podcast_id
-	AND date_start >= @start
-	AND date_start <= @end
-ORDER BY
-	date_start DESC, source_id ASC
+    account_id = @podcast_id 
+    AND date_start >= @start 
+    AND date_start <= @end 
+ORDER BY source_id ASC;
