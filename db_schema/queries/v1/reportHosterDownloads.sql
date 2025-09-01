90f1f3f82199c12f553f001fd753882bbc696ad8
Wolfgang Gassler <wolfgang@gassler.org>
2025-09-01 14:12:16 +0200

    lifetime_totals added

--- db_schema/queries/v1/reportHosterDownloads.sql
+++ db_schema/queries/v1/reportHosterDownloads.sql
@@ -7,6 +7,31 @@ WITH download_subdimensions AS (
     WHERE
         dim_name = 'complete'
     LIMIT 1
+),
+-- sum all monthly values
+-- monthly rows start on the first of a month and the end is the last day of the month
+total_downloads AS (
+    SELECT
+        account_id AS podcast_id,
+        hoster_id,
+        dimension,
+        subdimension,
+        SUM(value) AS lifetime_downloads
+    FROM
+        hosterPodcastMetrics
+    WHERE
+        account_id = @podcast_id
+        AND dimension = 'downloads'
+        AND subdimension = (SELECT dim_id FROM download_subdimensions)
+        AND DAYOFMONTH(start) = 1
+        AND YEAR(start) = YEAR(end)
+        AND MONTH(start) = MONTH(end)
+        AND DAYOFMONTH(end) = LAST_DAY(end)
+    GROUP BY
+        account_id,
+        hoster_id,
+        dimension,
+        subdimension
 )
 
 SELECT
@@ -15,13 +40,14 @@ SELECT
     dimension,
     subdimension,
     start,
-    value, -- daily value
+    value as daily_downloads, -- daily value
     -- total sum over daterange
     SUM(value) OVER (
         PARTITION BY account_id, hoster_id, dimension, subdimension
         ORDER BY start
         ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-    ) AS value_total
+    ) AS total_downloads,
+    (SELECT lifetime_downloads FROM total_downloads LIMIT 1) AS lifetime_downloads
 FROM
     hosterPodcastMetrics
 WHERE
