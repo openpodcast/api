bd24127862234066c076e378dd7b6fff5725c912
Matthias <matthias@endler.dev>
2025-09-01 15:00:47 +0200

    Remove unnecessary subquery for total impressions

--- db_schema/queries/v1/spotifyImpressionsFunnel.sql
+++ db_schema/queries/v1/spotifyImpressionsFunnel.sql
@@ -1,17 +1,12 @@
 -- Gets the latest 30-day funnel data (impressions -> considerations -> streams)
 -- Returns data for the most recent period within the requested date range
--- Each row includes the total impressions for reference
+-- Total impressions are available separately in the spotifyImpressions table
 SELECT
 	account_id,
 	date,
 	step_id,
 	step_count,
-	conversion_percent,
-	(SELECT step_count 
-	 FROM spotifyImpressionsFunnel sif2 
-	 WHERE sif2.account_id = spotifyImpressionsFunnel.account_id 
-	   AND sif2.date = spotifyImpressionsFunnel.date 
-	   AND sif2.step_id = 'impressions') as total_impressions
+	conversion_percent
 FROM
 	spotifyImpressionsFunnel
 WHERE
