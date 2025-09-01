65157da141f82de5ea0f5166d78399d3585a3856
Matthias Endler <matthias@endler.dev>
2025-09-01 18:26:30 +0200

    Add SQL query for hoster download metrics with 30-day and total downloads (#201)
    * Add SQL query for hoster download metrics with 30-day and total downloads
    * - added filter on subdimension (why is is complete?)
    - renamed as it just returns downloads
    * lifetime_totals added
    * Refactor SQL query for hoster downloads to improve date handling and clarity
    ---------
    
    Co-authored-by: Wolfgang Gassler <wolfgang@gassler.org>

---
+++ db_schema/queries/v1/reportHosterDownloads.sql
@@ -0,0 +1,56 @@
+-- get subdimension id for downloads
+WITH download_subdimensions AS (
+    SELECT
+        dim_id, dim_name
+    FROM
+        subdimensions
+    WHERE
+        dim_name = 'complete'
+    LIMIT 1
+),
+-- sum all monthly values
+-- monthly rows start on the first of a month and the end is the last day of the month
+total_downloads AS (
+    SELECT
+        account_id AS podcast_id,
+        hoster_id,
+        dimension,
+        subdimension,
+        SUM(value) AS lifetime_downloads
+    FROM
+        hosterPodcastMetrics
+    WHERE
+        account_id = @podcast_id
+        AND dimension = 'downloads'
+        AND subdimension = (SELECT dim_id FROM download_subdimensions)
+        -- monthly rows start on the first of a month and the end is the last day of the month
+        AND DAYOFMONTH(start) = 1 AND DATE(end) = LAST_DAY(start)
+    GROUP BY
+        account_id,
+        hoster_id,
+        dimension,
+        subdimension
+)
+
+SELECT
+    account_id AS podcast_id,
+    hoster_id,
+    dimension,
+    subdimension,
+    DATE(start) AS date,
+    value as daily_downloads, -- daily value
+    -- total sum over daterange
+    SUM(value) OVER (
+        PARTITION BY account_id, hoster_id, dimension, subdimension
+        ORDER BY start
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    ) AS total_downloads,
+    (SELECT lifetime_downloads FROM total_downloads LIMIT 1) AS lifetime_downloads
+FROM
+    hosterPodcastMetrics
+WHERE
+    account_id = @podcast_id 
+    AND dimension = 'downloads'
+    AND subdimension = (SELECT dim_id FROM download_subdimensions)
+    AND start = end
+    AND start BETWEEN @start AND @end;
