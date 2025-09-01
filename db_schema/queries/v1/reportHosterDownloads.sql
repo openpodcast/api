70a0664c346a80c148aeb35e6e140aad6c3c7283
Wolfgang Gassler <wolfgang@gassler.org>
2025-09-01 12:45:39 +0200

    - added filter on subdimension (why is is complete?) - renamed as it just returns downloads

---
+++ db_schema/queries/v1/reportHosterDownloads.sql
@@ -0,0 +1,36 @@
+-- Returns both total metrics and metrics for a given period (e.g., last 30 days)
+-- Only sums daily metrics (start = end)
+-- Covers downloads, platforms, clients, sources
+
+-- get subdimension id for downloads
+
+WITH download_subdimensions AS (
+    SELECT
+        dim_id, dim_name
+    FROM
+        subdimensions
+    WHERE
+        dim_name = 'complete'
+    LIMIT 1
+)
+
+SELECT
+    account_id AS podcast_id,
+    hoster_id,
+    dimension,
+    subdimension,
+    start,
+    value,
+    SUM(value) OVER (
+        PARTITION BY account_id, hoster_id, dimension, subdimension
+        ORDER BY start
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    ) AS value_total
+FROM
+    hosterPodcastMetrics
+WHERE
+    account_id = @podcast_id
+    AND dimension = 'downloads'
+    AND subdimension = (SELECT dim_id FROM download_subdimensions)
+    AND start = end
+    AND start BETWEEN @start AND @end;
