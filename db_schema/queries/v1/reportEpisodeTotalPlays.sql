a75307fdb243c119f4a1fbd048c446fe35df7878
Wolfgang Gassler <wolfgang@gassler.org>
2025-09-15 18:41:12 +0200

    Refactor reportEpisodeTotalPlays query to include hoster streams and improve total plays calculation

--- db_schema/queries/v1/reportEpisodeTotalPlays.sql
+++ db_schema/queries/v1/reportEpisodeTotalPlays.sql
@@ -1,4 +1,13 @@
-WITH spotify_streams AS (
+WITH download_subdimensions AS (
+    SELECT
+        dim_id
+    FROM
+        subdimensions
+    WHERE
+        dim_name = 'complete'
+    LIMIT 1
+),
+spotify_streams AS (
     SELECT
         episode_id,
         SUM(sps_streams) AS spotify_plays
@@ -23,17 +32,36 @@ apple_streams AS (
         AND account_id = @podcast_id
     GROUP BY
         episode_id
+),
+hoster_streams AS (
+    SELECT
+        episode_id,
+        SUM(value) AS hoster_plays
+    FROM
+        hosterEpisodeMetrics
+    WHERE
+        start >= @start
+        AND end <= @end
+        AND start = end -- daily data only
+        AND account_id = @podcast_id
+        AND dimension = 'downloads'
+        AND subdimension = (SELECT dim_id FROM download_subdimensions)
+    GROUP BY
+        episode_id
 )
 SELECT 
     E.account_id as podcast_id,
     E.guid,
     S.spotify_plays AS spotify_plays,
     A.apple_plays AS apple_plays,
-    S.spotify_plays + A.apple_plays as total_plays
+    H.hoster_plays AS hoster_plays,
+    COALESCE(S.spotify_plays, 0) + COALESCE(A.apple_plays, 0) + COALESCE(H.hoster_plays, 0) as total_plays
 FROM 
     episodeMapping E
-JOIN 
+LEFT JOIN 
     spotify_streams S ON E.spotify_episode_id = S.episode_id
-JOIN 
+LEFT JOIN 
     apple_streams A ON E.apple_episode_id = A.episode_id
+LEFT JOIN 
+    hoster_streams H ON E.hoster_episode_id = H.episode_id
 WHERE E.account_id = @podcast_id;
